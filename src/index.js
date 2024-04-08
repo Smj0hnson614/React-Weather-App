a66427ad2622149c9b7a1dcad85d2dd3eff66c56
Smj0hnson614 <153125119+Smj0hnson614@users.noreply.github.com>
2024-04-08 16:32:17 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <Weather />
+    </div>
+  </StrictMode>
+);
